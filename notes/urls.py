6f4ef5f176033d0cbee5b35e1888b8da9d5c8dde
BKS <simpavid@gmail.com>
2012-05-16 05:19:13 -0700

    testing urls and views

--- notes/urls.py
+++ notes/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'collegenotes.views.index'),
     # Examples:
     # url(r'^$', 'notes.views.home', name='home'),
     # url(r'^notes/', include('notes.foo.urls')),
